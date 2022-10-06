270fad2ff9089a90b7f4e691726432bf047c6406
Fatima Musa <mpatigul@my.centennialcollege.ca>
2022-10-06 15:47:19 -0400

    added Express, and third-party modules

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const http = require('http');
-/*
-const server = http.createServer((req, res) =>{
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello from NodeJS Application');
-});
-*/
 
+import express from "express";
+
+const app = express();
+
+const http = require('http');
+
+// Create the webserver event handler
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end('<h1>Hello World</h1>');
+});
+
+
+/*
 const server = http.createServer((req, res) =>{
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<h1>Hello from NodeJS Application as html</h1>');
 });
 
-/*
+
 const server = http.createServer((req, res) =>{
     res.writeHead(200, {'Content-Type':'application/json'});
    
@@ -21,5 +29,6 @@ const server = http.createServer((req, res) =>{
 server.listen(3000);
 
 console.log('Server running at http://localhost:3000');
+
 
 
